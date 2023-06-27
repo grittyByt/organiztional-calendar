6b77d5c857bc60d026b1087676ce10a151450032
grittyByt <terrell_whiting@yahoo.com>
2023-06-27 02:25:21 -0400

    added html file, css file, js file / added some code to the files / quiz questions not appearing for me though so I will work that out

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+function bestScores() {
+    // either get scores from localstorage or set to empty array
+    var highScores = JSON.parse(window.localStorage.getItem('bestScores')) || [];
+  
+    // sort highscores by score property in descending order
+    highScores.sort(function (a, b) {
+      return b.score - a.score;
+    });
+  
+    for (var i = 0; i < highScores.length; i += 1) {
+      // create li tag for each high score
+      var liTag = document.createElement('li');
+      liTag.textContent = highScores[i].initials + ' - ' + highScores[i].score;
+  
+      // display on page
+      var olEl = document.getElementById('bestScores');
+      olEl.appendChild(liTag);
+    }
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem('bestScores');
+    window.location.reload();
+  }
+  
+  document.getElementById('clear').onclick = clearHighscores;
+  
+  // run function when page loads
+  printHighscores();
+  
